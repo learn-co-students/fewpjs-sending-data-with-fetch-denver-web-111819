77175cfa5f585f002ddc3659062d733d7531f119
CarlKarlQarl <karlmatthes@gmail.com>
2020-01-13 13:27:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(userName, userEmail){
+    return fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => response.json())
+    .then(result => {
+        let body = document.querySelector("body")
+        body.innerHTML = result.id
+    })
+    .catch(error => {
+        let body = document.querySelector("body")
+        body.innerHTML = error.message
+    })
+}
